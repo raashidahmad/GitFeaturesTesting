8d6f81e49d2c4872bef6dcf5bb75f84eac101872
raashidahmad <rashid.ahmad@live.com>
2021-10-27 11:11:43 +0500

    Update Program.cs
    new changes in version 1

--- GitFeaturesTesting/Program.cs
+++ GitFeaturesTesting/Program.cs
@@ -7,6 +7,7 @@ namespace GitFeaturesTesting
         static void Main(string[] args)
         {
             Console.WriteLine("These are the changes in version1. I have updated it more. This is version 1. Changes from server into version 1");
+            Console.WriteLine("New changes in version 1");
         }
     }
 }
