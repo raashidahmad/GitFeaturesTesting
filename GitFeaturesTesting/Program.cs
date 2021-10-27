2f268ebb81dd29114cd59f37bde910fecb8e4e8a
Rashid Ahmad <raashid.ahmad@gmail.com>
2021-10-27 11:27:59 +0500

    create branch and changes in version 1

--- GitFeaturesTesting/Program.cs
+++ GitFeaturesTesting/Program.cs
@@ -8,6 +8,7 @@ namespace GitFeaturesTesting
         {
             Console.WriteLine("These are the changes in version1. I have updated it more. This is version 1. Changes from server into version 1");
             Console.WriteLine("New changes in version 1");
+            Console.WriteLine("Some more changes in the local branch. To create branch and changes both");
         }
     }
 }
